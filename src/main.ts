d9ae2f5ebcdb455d9d71c719e8b0e7bf3bfb19b0
mameru-carr <samuelcarr.official@gmail.com>
2024-08-11 18:13:01 +0530

    Setup tailwind and delete a bunch of stuff

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// tailwind css
+import './assets/tailwind.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
